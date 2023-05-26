3ba2f3c97d8901846894139f08ac00a241e5bde9
vanshajbajaj <78155493+vanshajbajaj@users.noreply.github.com>
2023-05-26 20:01:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const port=8000;
-const express=require('express');
+const port = 8000;
+const express = require('express');
+const requestIp = require('request-ip');
 
-const app=express();
+const app = express();
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
 
     // res.send("hello");
-    const ipAddress = req.socket.remoteAddress;
-    res.send(ipAddress);
+    // const ipAddress = req.socket.remoteAddress;
+    // res.send(ipAddress);
+    var clientIp = requestIp.getClientIp(req)
+    res.send(`Your IP Address is ${clientIp}.`)
 
 })
 
-app.listen(port,(err)=>{
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         console.log(err);
     }
-    console.log("Server is up at: ",port);
+    console.log("Server is up at: ", port);
 })
